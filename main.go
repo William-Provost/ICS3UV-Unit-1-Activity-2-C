c5aecc4114c0a560722e1ba4ed3a1106d5e2b6b6
default <william.provost@stu.ocsb.ca>
2025-09-24 13:19:54 +0000

    added main.go file

--- main.go
+++ main.go
@@ -1,1 +1,27 @@
-package williamprovost
+/**
+ * @author William Provost
+ * @version 1.0.0
+ * @date 2025-09-24
+ * @fileoverview This program pratices with string literals.
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	//In programming you often use // in front of text to tell
+	// the computor not to interpret or compile these lines
+	// Programmers call this comments. 
+
+	//string literal text
+	fmt.Println("I love Computer Science!!")
+
+	//string literal text
+	fmt.Println("    I  love Computer Science")
+
+	//string literal text
+	fmt.Println("I   love   Computer   Science   100%")
+
+	fmt.Println("\nDone.")
+}
