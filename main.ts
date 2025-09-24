54158decdb2424cdaa2e8a79d34befa2aa0dd710
default <william.provost@stu.ocsb.ca>
2025-09-24 13:12:58 +0000

    added I love Computor Science string

---
+++ main.ts
@@ -0,0 +1,17 @@
+/**
+ * @author William Provost
+ * @version 1.0.0
+ * @date 2025-09-24
+ * @fileoverview This program pratices with string literals.
+ */
+
+// string literal text
+console.log("I love Computer Science!!");
+
+//string literal text
+console.log("    I  love Computer Science");
+
+//string literal text
+console.log("I   love   Computor   Science   100%");
+
+console.log("\nDone.");
